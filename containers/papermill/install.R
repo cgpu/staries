6e965a8a0565804cbab1a38220ff53d8b8928d3d
cgpu <38183826+cgpu@users.noreply.github.com>
2021-08-19 13:51:13 +0100

    Update install.R

--- containers/papermill/install.R
+++ containers/papermill/install.R
@@ -1,4 +1,4 @@
 Sys.setenv(TAR = "/bin/tar")
 options(repos = "https://cloud.r-project.org/")
 install.packages("remotes")
-remotes::install_version('ggpubr', version = '0.4.0')
+remotes::install_version('ggpubr')
